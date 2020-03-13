d51a762d2a0b752a080f339b75da3d2e0165a8a3
God-Ra <smitran.godra@gmail.com>
2020-03-13 15:23:43 +0100

    Added login feature

--- Basic-file-system/Source.cpp
+++ Basic-file-system/Source.cpp
@@ -1,13 +1,67 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <Windows.h>
 #include "gline.h"
 
+/*
+==========
+isUsernameValid
+
+The function checks whether pair of (username, password) exist in folder "users.txt"
+returns 1 if the pair exists, returns 0 otherwise
+The function does not check whether the file "users.txt" is correctly written, takes it as a given
+If the file does not exist the function still works
+==========
+*/
+bool isUsernameValid(const std::string& username, const std::string& password);
 
 int main()
 {
-	std::string input = "";
-
-	while (input != "g")
+	std::string username;
+	while (1)
 	{
-		getLineAsterisks(input, 31, "Password: ");
+		std::cout << ">>";
+		std::string input;
+		std::getline(std::cin, input);
+
+		if (input == "login")
+		{
+			std::string password;
+			getLine(username, 31, "Username: ");
+			getLineAsterisks(password, 31, "Password: ");
+
+			if (isUsernameValid(username, password))
+			{
+				//Start the normal operation of the program
+				std::cout << "Should start the normal operation of the program";
+				Sleep(1000);
+				return 0;
+			}
+			else
+			{
+				std::cout << "\nUsername or password are invalid!\n\n";
+			}
+		}
 	}
+}
+
+bool isUsernameValid(const std::string& username, const std::string& password)
+{
+	std::ifstream fileUsers;
+	fileUsers.open("users.txt", std::ios::in);
+
+	bool isUserFound = false;
+	std::string fileUsername = "", filePassword = "";
+	while (fileUsers.good() && !isUserFound)
+	{
+		std::getline(fileUsers, fileUsername);
+		std::getline(fileUsers, filePassword);
+
+		if (fileUsername == username && filePassword == password)
+			isUserFound = true;
+	}
+
+	fileUsers.close();
+	return isUserFound;
 }
